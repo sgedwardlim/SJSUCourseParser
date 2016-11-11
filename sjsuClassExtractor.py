86ff4f4051bfcacd5d5c2434931c76114cc7fff8
Edward Lim <sgedwardlim@gmail.com>
2016-11-11 00:17:57 -0800

    Bug fix: Regex Expression Was not capturing fields with empty professors

--- sjsuClassExtractor.py
+++ sjsuClassExtractor.py
@@ -5,19 +5,16 @@ import csv
 
 # Create the lecture regex
 
-lectureRegex = re.compile('''
 
-        # Looks for the class code matching pattern
-        # eg. AFAM 002A
-        ([A-Z]+[\s][0-9]+[A-Z]*)
-        # Looks for the name of the class
-        (.+)
-        [\s][0-9]+[\s][0-9]+
-        # Looks for everything and any character
-        .+
-        # Looks for the full name of the teacher matching pattern
-        # eg. L ROGER-GEE
-        ([A-Z][\s][A-Z]+[-]*[A-Z]+)[\n]
+lectureRegex = re.compile('''
+        ([A-Z]+\s\d+[A-Z]*)
+        (.+?)
+        \s\d+\s\d+.+?
+        (\d\d\/\d\d\/\d\d)
+        -
+        (\d\d\/\d\d\/\d\d)
+        \s
+        (?:.*?([A-Z]\s[A-Z]+-*[A-Z]+))?
 ''', re.VERBOSE)
 
 # create a new csv file in the same directory
@@ -25,12 +22,12 @@ outputFile = open('misc/output.csv', 'w', newline='')
 outputWriter = csv.writer(outputFile)
 
 # Open file that contains the Data to be parsed in .txt format
-file = open('misc/decodedata.txt', 'r')
+file = open('misc/sjsuclassdata-utf8.txt', 'r')
 
 regexMatches = lectureRegex.findall(file.read())
 
 for match in regexMatches:
     print(match[1])
-    outputWriter.writerow([match[0], match[1], match[2]])
+    outputWriter.writerow([match[0], match[1], match[2], match[3], match[4]])
 
 outputFile.close()
